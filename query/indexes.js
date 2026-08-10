4ea3be4b3a645d01a858268d5c929b05f9951c75
bmestres <132062905+bmestres@users.noreply.github.com>
2026-08-10 14:43:32 +0200

    Fix index creation syntax for grades and location

--- query/indexes.js
+++ query/indexes.js
@@ -4,6 +4,6 @@ db.restaurants.createIndex({ cuisine: 1 });
 
 db.restaurants.createIndex({ borough: 1 })
 
-db.restaurants.createIndex({ grades.score: 1 });
+db.restaurants.createIndex({ 'grades.score': 1 });
 
-db.restaurants.createIndex({ location.coordinates: 1 });
+db.restaurants.createIndex({ 'location.coordinates': 1 });
