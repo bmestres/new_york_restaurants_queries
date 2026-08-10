80d252fe9290a481932c23602116762cf5267891
bmestres <132062905+bmestres@users.noreply.github.com>
2026-08-10 15:10:38 +0200

    Add new indexes for location and grades fields

--- query/indexes.js
+++ query/indexes.js
@@ -6,6 +6,16 @@ db.restaurants.createIndex({ cuisine: 1 });
 
 db.restaurants.createIndex({ borough: 1 })
 
-db.restaurants.createIndex({ 'grades.score': 1 });
+db.restaurants.createIndex({ 'location.type': 1 });
 
 db.restaurants.createIndex({ 'location.coordinates': 1 });
+
+db.restaurants.createIndex({ 'grades.date': 1 })
+
+db.restaurants.createIndex({ 'grades.grade': 1 });
+
+db.restaurants.createIndex({ 'grades.score': 1 });
+
+
+
+
