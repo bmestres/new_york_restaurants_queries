ce86105aaf387af03ca369b981781dbf3196573f
bmestres <132062905+bmestres@users.noreply.github.com>
2026-08-10 14:52:17 +0200

    Fix spelling of 'Brooklyn' in queries

--- query/queries.js
+++ query/queries.js
@@ -50,10 +50,10 @@ db.restaurants.find({name: /Reg/}, {_id: 0, restaurant_id: 1, name: 1, borough: 
 db.restaurants.find({borough: "Bronx", $or: [{cuisine: "American"},{cuisine: "Chinese"}]}, {_id: 0})
 
 // 18. Trobar restaurant_id, name, borough i cuisine per a Staten Island, Queens, Bronx o Brooklyn.
-db.restaurants.find({borough: {$in: ["Staten Island","Queens", "Bronx", "Brooklin"]}}, {_id: 0, restaurant_id: 1, name: 1, borough: 1, cuisine: 1})
+db.restaurants.find({borough: {$in: ["Staten Island","Queens", "Bronx", "Brooklyn"]}}, {_id: 0, restaurant_id: 1, name: 1, borough: 1, cuisine: 1})
 
 // 19. Trobar restaurant_id, name, borough i cuisine per a restaurants que NO són d'aquests barris.
-db.restaurants.find({borough: {$nin: ["Staten Island","Queens", "Bronx", "Brooklin"]}}, {_id: 0, restaurant_id: 1, name: 1, borough: 1, cuisine: 1})
+db.restaurants.find({borough: {$nin: ["Staten Island","Queens", "Bronx", "Brooklyn"]}}, {_id: 0, restaurant_id: 1, name: 1, borough: 1, cuisine: 1})
 
 // 20. Trobar restaurant_id, name, borough i cuisine amb marcador no superior a 10.
 db.restaurants.find({'grades.score': {$lte: 10}}, {_id: 0, restaurant_id: 1, name: 1, borough: 1, cuisine: 1})
