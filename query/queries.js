cba441c7d0922e5d7f1d56446a982c2d91b89394
bmestres <132062905+bmestres@users.noreply.github.com>
2026-08-10 13:46:15 +0200

    Fix typo in restaurant_id field in query

--- query/queries.js
+++ query/queries.js
@@ -38,7 +38,7 @@ db.restaurants.find({cuisine: {$ne: "American"},'grades.score': {$gt: 70},'locat
 db.restaurants.find({cuisine: {$ne: "American"},'grades.grade': "A", borough: {$ne: "Brooklyn"}}, {_id: 0}).sort({cuisine: -1})
 
 // 14. Trobar restaurant_id, name, borough i cuisine on el nom comença amb 'Wil'.
-db.restaurants.find({name: /^Wil/}, {_id: 0, resataurant_id: 1, name: 1, borough: 1, cuisine: 1})
+db.restaurants.find({name: /^Wil/}, {_id: 0, restaurant_id: 1, name: 1, borough: 1, cuisine: 1})
 
 // 15. Trobar restaurant_id, name, borough i cuisine on el nom acaba en 'ces'.
 db.restaurants.find({name: /ces$/}, {_id: 0, restaurant_id: 1, name: 1, borough: 1, cuisine: 1})
